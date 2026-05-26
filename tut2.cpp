276bab45366a95c3c93a328cc3551f494c0f1e26
roushan-singh11 <roushansingh8310@gmail.com>
2026-05-26 21:51:37 +0530

    Add files via upload

---
+++ tut2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std; //ab like cout ke phle std:: nhi lagana pdega...
+
+int main(){
+    cout<<"haa working hai, sikh gya namespcae use karna!!";
+    return 0;
+}
