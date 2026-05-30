9aa6f1058a91aea603ace400fca11520d07d90e6
roushan-singh11 <roushansingh8310@gmail.com>
2026-05-30 19:26:36 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+//so just learning about comments
+
+/* this is also a comment*/
+
+using namespace std;
+
+int main(){
+  int sum=6;
+  cout<<sum;
+
+  float sum1=12.6;
+  cout<<sum1;
+
+  
+  cout<<"hello world";
+
+  return 0;
+
+}
