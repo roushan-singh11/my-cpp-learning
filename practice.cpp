eeb54fb62e4afca65b34fe74fd045623a456226a
roushan-singh11 <roushansingh8310@gmail.com>
2026-05-30 19:29:29 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+//to calculate the product of 2 numbers
+
+using namespace std;
+
+int main(){
+
+    float num1;
+    float num2;
+
+    cout<<"enter the first number: \n";
+    cin>>num1;
+
+    cout<<"enter the second number: \n";
+    cin>>num2;
+
+    float product=num1*num2;
+    cout<<"the product of given numbers is: \n"<<product;
+
+
+    
+    return 0;
+
+}
