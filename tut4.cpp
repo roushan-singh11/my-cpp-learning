23f8b0f3e2c5a2b420f4a798f678f7ccf246c61f
roushan-singh11 <roushansingh8310@gmail.com>
2026-05-30 19:27:58 +0530

    Add files via upload

---
+++ tut4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a=14;
+    int b=15;
+    float pi=3.14;
+    char c='d';
+    double pi_extended=3.1425623;
+
+    
+    cout<<"\nthis is tut4!\n"<<"the value of a is "<<a <<".\nthe value of b is "<<b;
+
+    cout<<"the value of c is "<<c;
+
+    cout<<"\nthe value of pi is "<<pi;
+
+    cout<<"\nthe value of pi extended is: "<<pi_extended;
+    return 0;
+
+
+
+}
+
+//so learnt using 4 datatypes in here and using "" statments and << to hook variables together, along with when to use \n
+
